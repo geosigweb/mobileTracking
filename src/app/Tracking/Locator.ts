1b1abdec6cc8fa03a44b1d0a6ef362b023eb0fdb
f.taillefer <f.taillefer@geosigweb.fr>
2018-10-24 18:41:19 +0200

    bug : essai resolution probleme test

--- src/app/Tracking/Locator.ts
+++ src/app/Tracking/Locator.ts
@@ -7,10 +7,11 @@ import { HubConnection } from '@aspnet/signalr';
 import { Subscription } from 'rxjs';
 import { Events } from '@ionic/angular';
 import 'rxjs/add/operator/filter';
+import { AppComponent } from '../app.component';
 
 @Component({})
 export class Locator {
-    private hubConnection: HubConnection;
+    private appComponent: AppComponent;
     private trackFile : string;
     private idVehicule: number;
     private idParcours: number;
@@ -50,27 +51,35 @@ export class Locator {
         });
     }
 
-    async Initialize(connection, idVehicule, idParcours)
+    async Initialize(appComponent, idVehicule, idParcours)
     {
-        try {
-            this.hubConnection = connection;
-            this.idVehicule = idVehicule;
-            this.idParcours = idParcours;
+        this.appComponent = appComponent;
+        this.idVehicule = idVehicule;
+        this.idParcours = idParcours;
+    }
 
+    async GetTrackFileName()
+    {
+        try 
+        {
             var urlAPI = 'https://geosigwebcd19webapi.azurewebsites.net/api/Layers'
             var application = 'CD19 Routes 4.0';
-            var url = `${urlAPI}/${application}/newTrack/${idParcours}`;
+            var url = `${urlAPI}/${application}/newTrack/${this.idParcours}`;
             let {value} = await this.fetchAsync(url);
-            this.trackFile = value;
-            this.events.publish("gwInfo", `Received track File name ${this.trackFile}`);
-        } catch (error) {
+            this.events.publish("gwInfo", `Received track File name ${value}`);
+            return value;
+        } 
+        catch (error) 
+        {
             this.events.publish("gwError", error);
+            return "";
         }
     }
 
-    StartTracking()
+    async StartTracking()
     {
         try {
+            this.trackFile = await this.GetTrackFileName();
             var options = { enableHighAccuracy: false };
             this.watcher = this.geolocation.watchPosition(options).filter((p) => p.coords !== undefined).subscribe(pos => this.Watch(pos));
         } catch (error) {
@@ -78,27 +87,44 @@ export class Locator {
         }
     }
 
-    Test()
-    {
-        try {
-            this.hubConnection.invoke("SendMessage",  this.trackFile, this.idVehicule, this.idParcours,"0", "0");
-            this.events.publish("gwInfo", "Test executed");
-        } catch (error) {
-            this.events.publish("gwError", error);
-        }
-    }
-
-    StopTracking() 
+    async StopTracking() 
     { 
         try {
             this.watcher.unsubscribe();
-            this.hubConnection.invoke("SendMessage",  this.trackFile, this.idVehicule, this.idParcours,"Infinite", "Infinite");
+            var connection = await this.appComponent.GetConnection();
+            await connection.invoke("SendMessage",  this.trackFile, this.idVehicule, this.idParcours,"Infinite", "Infinite");
             this.events.publish("gwInfo", "Tracking stopped");
         } catch (error) {
             this.events.publish("gwError", error);
         }
     }
 
+    async Test()
+    {
+        try 
+        {
+            await this.appComponent.TestConnection(this.trackFile, this.idVehicule, this.idParcours);
+        } 
+        catch (error) 
+        {
+            this.events.publish("gwInfo", "Test() " + error.toString());
+        }
+    }
+
+    async Watch(pos: Geoposition)
+    {
+        try {
+            let lat: string = pos.coords.latitude.toString();
+            let lon: string = pos.coords.longitude.toString();
+            this.events.publish("gwInfo", `Watch Sending : lat: ${lat}, lon: ${lon}...`);
+            var connection = await this.appComponent.GetConnection();
+            await connection.invoke("SendMessage", this.trackFile, this.idVehicule, this.idParcours, lat, lon);
+            this.events.publish("gwInfo", `Sent!`);
+        } catch (error) {
+            this.events.publish("gwError", error);
+        }
+    }    
+    
     async fetchAsync (url) 
     {
         try {
@@ -111,16 +137,5 @@ export class Locator {
         }
    }
 
-    async Watch(pos: Geoposition)
-    {
-        try {
-            let lat: string = pos.coords.latitude.toString();
-            let lon: string = pos.coords.longitude.toString();
-            this.events.publish("gwInfo", `Watch Sending : lat: ${lat}, lon: ${lon}`);
-            await this.hubConnection.invoke("SendMessage", this.trackFile, this.idVehicule, this.idParcours, lat, lon);
-            this.events.publish("gwInfo", `Watch Sent : lat: ${lat}, lon: ${lon}`);
-        } catch (error) {
-            this.events.publish("gwError", error);
-        }
-    }
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { LogLevel, HubConnectionBuilder, HubConnection } from '@aspnet/signalr';
 
 import { Platform } from '@ionic/angular';
+import { Events } from '@ionic/angular';
+
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import {Locator} from '../app/Tracking/Locator';
@@ -14,6 +16,7 @@ import {Locator} from '../app/Tracking/Locator';
 export class AppComponent {
   private hubConnection: HubConnection;
   constructor(
+    public events : Events,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
@@ -22,21 +25,48 @@ export class AppComponent {
     this.initializeApp();
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+  public async GetConnection()
+  {
+    if (this.hubConnection === undefined)
+    {
+      try {
+        this.hubConnection= new HubConnectionBuilder()
+          .withUrl('https://geosigwebcd19webapi.azurewebsites.net/trackingHub')
+          .configureLogging(LogLevel.Information)
+          .build();
+        await this.hubConnection.start();
+      } catch (error) {
+        this.events.publish("gwError", error.message);
+      }
+    }
+    return this.hubConnection;
+  }
+
+  public async TestConnection(trackFile, idVehicule, idParcours)
+  {
+    if (this.hubConnection === undefined)
+    {
       this.hubConnection= new HubConnectionBuilder()
           .withUrl('https://geosigwebcd19webapi.azurewebsites.net/trackingHub')
           .configureLogging(LogLevel.Information)
           .build();
-      this.hubConnection.start()
-          .then(()=>{ 
-            console.log("connection ok"); 
-            this.locator.Initialize(this.hubConnection, 1,6); 
-          })
-          .catch(err => console.error("could not connect :" + err.toString()));
-      
+          
+      try {
+        await this.hubConnection.start();
+        await this.hubConnection.invoke("SendMessage",  trackFile, idVehicule, idParcours,"0", "0");
+      } catch (error) {
+        this.events.publish("gwInfo", 'Start Operation failed because ' + error);
+      } 
+     }
+  }
+
+
+  initializeApp() {
+    this.platform.ready().then(() => 
+    {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      this.locator.Initialize(this, 1,6);
     });
   }
 
